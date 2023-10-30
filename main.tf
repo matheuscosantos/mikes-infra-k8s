93db898a93240c69e6a78b5d5d8ef2ea546e029e
GAPrado <guilhermeprado@outlook.com>
2023-10-30 13:11:54 -0300

    add fargate profile

--- main.tf
+++ main.tf
@@ -2,14 +2,37 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "mikes-terraform-state"
-    key            = "mikes-cluster.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+## - cloudwatch log
+
+resource "aws_cloudwatch_log_group" "log" {
+  name              = "/aws/eks/${var.cluster_name}"
+  retention_in_days = 7
+}
+
+## - fargate profile
+
+resource "aws_iam_role" "eks_fargate-profile-role" {
+  name = "eks-fargate-profile-role"
+  assume_role_policy = file("policy/fargate_profile_assume_role_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "eks_fargate-profile-policy-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks_fargate-profile-role.name
+}
+
+resource "aws_eks_fargate_profile" "eks_fargate_profile" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "fargate-profile"
+  pod_execution_role_arn = aws_iam_role.eks_fargate-profile-role.arn
+  subnet_ids             = var.subnet_ids
+
+  selector {
+    namespace = var.cluster_name
   }
 }
+
+#-- eks
 
 resource "aws_iam_role" "role" {
   name               = "${var.cluster_name}-role"
@@ -21,12 +44,7 @@ resource "aws_iam_role_policy_attachment" "policy-attachment" {
   role       = aws_iam_role.role.name
 }
 
-resource "aws_cloudwatch_log_group" "log" {
-  name              = "/aws/eks/${var.cluster_name}"
-  retention_in_days = 7
-}
-
-resource "aws_eks_cluster" "mikes-cluster" {
+resource "aws_eks_cluster" "cluster" {
   name      = var.cluster_name
   role_arn  = aws_iam_role.role.arn
 
@@ -41,3 +59,4 @@ resource "aws_eks_cluster" "mikes-cluster" {
     aws_cloudwatch_log_group.log
   ]
 }
+
