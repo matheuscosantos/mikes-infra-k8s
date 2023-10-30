9e11fded13470cccfb5c9e1c7c6078ed65a24a71
GAPrado <guilhermeprado@outlook.com>
2023-10-30 13:18:48 -0300

    add fargate profile

--- main.tf
+++ main.tf
@@ -11,42 +11,47 @@ resource "aws_cloudwatch_log_group" "log" {
 
 ## - fargate profile
 
-resource "aws_iam_role" "eks_fargate-profile-role" {
+resource "aws_iam_role" "eks_fargate_profile_role" {
   name = "eks-fargate-profile-role"
   assume_role_policy = file("policy/fargate_profile_assume_role_policy.json")
 }
 
-resource "aws_iam_role_policy_attachment" "eks_fargate-profile-policy-attachment" {
+resource "aws_iam_role_policy_attachment" "eks_fargate_profile_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.eks_fargate-profile-role.name
+  role       = aws_iam_role.eks_fargate_profile_role.name
 }
 
 resource "aws_eks_fargate_profile" "eks_fargate_profile" {
   cluster_name           = aws_eks_cluster.cluster.name
   fargate_profile_name   = "fargate-profile"
-  pod_execution_role_arn = aws_iam_role.eks_fargate-profile-role.arn
+  pod_execution_role_arn = aws_iam_role.eks_fargate_profile_role.arn
   subnet_ids             = var.subnet_ids
 
   selector {
     namespace = var.cluster_name
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_fargate_profile_policy_attachment,
+    aws_eks_cluster.cluster
+  ]
 }
 
 #-- eks
 
-resource "aws_iam_role" "role" {
+resource "aws_iam_role" "eks_role" {
   name               = "${var.cluster_name}-role"
   assume_role_policy = file("policy/eks_assume_role_policy.json")
 }
 
-resource "aws_iam_role_policy_attachment" "policy-attachment" {
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.role.name
+  role       = aws_iam_role.eks_role.name
 }
 
 resource "aws_eks_cluster" "cluster" {
   name      = var.cluster_name
-  role_arn  = aws_iam_role.role.arn
+  role_arn  = aws_iam_role.eks_role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
@@ -55,7 +60,7 @@ resource "aws_eks_cluster" "cluster" {
   enabled_cluster_log_types = ["api", "audit"]
 
   depends_on = [
-    aws_iam_role_policy_attachment.policy-attachment,
+    aws_iam_role_policy_attachment.eks_policy_attachment,
     aws_cloudwatch_log_group.log
   ]
 }
